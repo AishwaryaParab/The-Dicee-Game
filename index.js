a8c4138b32f1c6fab077c5ad66035de8ad6c051e
Aishwarya Parab <65883954+AishwaryaParab@users.noreply.github.com>
2021-06-28 16:20:31 +0530

    updated index.js image links

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var randomNumber1 = (Math.floor((Math.random())*6))+1;
 var randomNumber2 = (Math.floor((Math.random())*6))+1;
-var randomImage1 = "images/dice" + randomNumber1 + ".png";
-var randomImage2 = "images/dice" + randomNumber2 + ".png";
+var randomImage1 = "/images/dice" + randomNumber1 + ".png";
+var randomImage2 = "/images/dice" + randomNumber2 + ".png";
 
 /*for the first dice*/
 document.querySelector(".img1").setAttribute("src",randomImage1);
